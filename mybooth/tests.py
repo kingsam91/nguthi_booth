1c1c3f2fedb5d119c450c7e086a960d3b66a13f3
kingsam91 <lambetgm@gmail.com>
2020-08-24 07:29:37 +0300

    add test class to test category

--- mybooth/tests.py
+++ mybooth/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from .models import Category,Locatoin,Image
 
 # Create your tests here.
+class CategoryTestClass(TestCase):
+
+    #Set up method
+    def setUp(self):
+        self.burger = Category(
+            name='Food'
+        )
+
+    #Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.burger, Category))
+
+    #Testing save method
+    def test_save_method(self):
+        self.burger.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)
+
+    def tearDown(self):
+        Category.objects.all().delete()
