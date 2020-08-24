f17bc55c348e3288e09a1a63a99bc0aff33bd9d5
kingsam91 <lambetgm@gmail.com>
2020-08-24 07:20:06 +0300

    include location and category in image model

--- mybooth/models.py
+++ mybooth/models.py
@@ -18,4 +18,8 @@ class Image(models.Model):
     image = models.TextField()
     name = models.CharField(max_length=30)
     description = models.TextField()
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, default=1)
+    locatoin = models.ForeignKey(
+        Locatoin, on_delete=models.SET_NULL, null=True, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
