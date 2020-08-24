c66466bb3c4e4f659a5b6ac13638e056971857e4
kingsam91 <lambetgm@gmail.com>
2020-08-24 06:47:11 +0300

    add static for css

--- mybooth/urls.py
+++ mybooth/urls.py
@@ -1,6 +1,9 @@
+from django.urls import include, path
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url('^$',views.welcome, name='welcome')
+    # url('^today_photos$',views.today_photos, name='todayPhotos')
+    # url('^past_photos/(\d{4}-\d{2}-\d{2})/$',views.past_p hotos,name = 'pastPhotos') 
 ]
--- mybooth/views.py
+++ mybooth/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+  
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
 
 def welcome(request):
-    return HttpResponse('Welcome to the Nguthi booth')
+    # return render(request, 'welcome.html')
+    return render(request, 'all-photos/past_photos.html', {"date": "xxx"})
+
+# def today_photos(request):
+#     # return HttpResponse('Welcome to the Nguthi booth')
+#     date = dt.date.today()
+#     return render(request, 'all-photos/base.html', {"date": date,})
+
+# def past_photos(request, past_date):
+#     # return HttpResponse('Welcome to the Nguthi booth')
+#     try:
+#         date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+#     except ValueError:
+#         # Raise 404 error when ValueError is thrown
+#         raise Http404()
+#         assert False
+
+#     if date == dt.date.today():
+#         return redirect(today_photos)
+
+#     return render(request, 'all-photos/base.html', {"date": date,})
