2da380234f74e807d99bb372174b077d02caebb6
kingsam91 <lambetgm@gmail.com>
2020-08-24 07:04:59 +0300

    add location model

--- mybooth/models.py
+++ mybooth/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class locatoin(models.Model):
+    name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        returnself.name
